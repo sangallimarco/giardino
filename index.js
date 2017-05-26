b456bbf5dc57d855d70821a8e47c023f3aea73a6
Marco Sangalli <sangalli.marco@gmail.com>
2017-05-26 22:13:32 +0200

    adding right timings

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const queue = require('./libs/queue');
 const gpio = require('./libs/gpio');
 const pins = [7, 11, 13, 15, 19];
-const repeat = 10000;
-const delayOn = 2000;
-const delayOff = 1000;
+
+const sec = 1000;
+const min = sec * 60;
+const repeat = min * 60 * 6; // every 6 hours
+const delayOn = min * 10; // 10 mins
+const delayOff = sec * 10; // 10 secs
 
 const q = new queue();
 q.on('change', (pin, state) => {
-        console.log(pin, state);
+        console.log('Set Pin', pin, state);
         gpio.setPin(pin, state);
 });
 q.on('end', () => {
         console.log('Ended');
-
-        // restart
         setTimeout(() => {
                 start();
         }, repeat);
