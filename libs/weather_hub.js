837c6e8553cddbcc3724c30339d45fa811f1d74a
Marco Sangalli <sangalli.marco@gmail.com>
2017-05-29 21:14:12 +0200

    adding config

--- libs/weather_hub.js
+++ libs/weather_hub.js
@@ -1,57 +1,34 @@
+const config = require('config');
 const queue = require('./queue');
 const develop = process.env.NODE_ENV === 'development';
 
 const gpio = develop ? require('./gpio_mock') : require('./gpio');
-
-const config = require('config');
-const Forecast = require('forecast');
 
 const pins = config.get('CORE.PINS');
 const when = config.get('CORE.WHEN');
 const delayOn = config.get('CORE.ON');
 const delayOff = config.get('CORE.OFF');
 
+const Forecast = require('forecast');
 const forecastKey = config.get('FORECAST.KEY');
 const forecastLatLon = config.get('FORECAST.LATLON');
+const forecast = new Forecast({
+    service: 'darksky',
+    key: forecastKey,
+    units: 'celcius',
+    cache: true, // Cache API requests 
+    ttl: {
+        minutes: 27,
+        seconds: 45
+    }
+});
 
 class WeatherHub {
     constructor() {
         this.lock = false;
-    }
+        this.forecast = null;
 
-    run() {
-        let now = new Date();
-        let h = now.getHours();
-        let m = now.getMinutes();
-
-        // trigger
-        if (when.includes(h) && m === 0) {
-            // now get the forecast
-            this.forecast.get(forecastLatLon, (err, weather) => {
-                if (err) {
-                    // try again
-                    setTimeout(() => {
-                        this.run();
-                    }, 10000);
-                    return console.dir(err);
-                }
-
-                if (weather) {
-                    this.initQueue();
-                }
-            });
-
-        } else {
-            setTimeout(() => {
-                this.run();
-            }, 1000);
-        }
-    }
-
-    initQueue() {
-        this.lock = true;
-
-        let q = new queue(pins, delayOn, delayOff);
+        let q = new queue();
         q.on('change', (pin, state) => {
             this.setPin(pin, state);
         });
@@ -60,25 +37,54 @@ class WeatherHub {
             this.lock = false;
             this.run();
         });
-
-        console.log('Running Queue...');
-        q.run();
-
+        
         // destroy queue later
         this.q = q;
     }
 
-    initForecast() {
-        this.forecast = new Forecast({
-            service: 'darksky',
-            key: forecastKey,
-            units: 'celcius',
-            cache: true, // Cache API requests 
-            ttl: {
-                minutes: 27,
-                seconds: 45
+    run() {
+        if (!this.lock) {
+            let now = new Date();
+            let h = now.getHours();
+            let m = now.getMinutes();
+
+            // trigger
+            console.dir('Checking Time...');
+            if ((when.includes(h) && m === 0) || develop) {
+                // now get the forecast
+                console.dir('Checking Weather...');
+                forecast.get(forecastLatLon, (err, weather) => {
+                    if (err) {
+                        // try again
+                        setTimeout(() => {
+                            this.run();
+                        }, 10000);
+                        return console.dir(err);
+                    }
+
+                    // data returned now check
+                    let {
+                        cloudCover,
+                        temperature
+                    } = weather.currently;
+
+                    if (cloudCover < 1 && temperature > 5) {
+                        this.restartQueue();
+                    }
+                });
+
+            } else {
+                setTimeout(() => {
+                    this.run();
+                }, 5000);
             }
-        });
+        }
+    }
+
+    restartQueue() {
+        this.lock = true;
+        this.q.init(pins, delayOn, delayOff);
+        this.q.run();
     }
 
     setPin(pin, state) {
@@ -91,7 +97,7 @@ class WeatherHub {
         gpio.init(pins)
             .then(
                 () => {
-                    this.initQueue();
+                    this.run();
                 }
             );
     }
