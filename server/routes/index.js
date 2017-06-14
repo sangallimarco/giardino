1ccc132c3a13c77bac05d76d6e95e5c8442cf97e
Marco Sangalli <sangalli.marco@gmail.com>
2017-06-14 21:40:37 +0200

    adding mailer

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,24 @@
 const {Router} = require('express');
 const router = Router();
 const config = require('config');
+const oneceler = require('onceler');
+const mailer = require('../libs/mailer');
 
 router.post('/otp', (req, res, next) => {
     console.log(req.body);
+    let {email} = req.body;
 
-    res.json({});
+    if (config.get('ALLOWED').includes(email)) {
+        let token = oneceler.TOTP(config.get('TOTP'));
+        let sender = new mailer('garder@local', email);
+        sender.
+        res.json({status: true});
+    } else {
+        res
+            .status(403)
+            .json({});
+    }
+
 });
 
 router.post('/auth', (req, res, next) => {
